6a7b5f4d9ae55923751797ef6a6e82b2f7c34a3e
kipkorir73 <123023142+kipkorir73@users.noreply.github.com>
2023-07-25 01:12:37 +0300

    The first commit

---
+++ App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import TransactionTable from "./components/TransactionTable";
+import TransactionForm from "./components/TransactionForm";
+
+const App = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions")
+      .then((response) => response.json())
+      .then((data) => {
+        setTransactions(data);
+        setFilteredTransactions(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  const handleAddTransaction = (newTransaction) => {
+    setFilteredTransactions([...filteredTransactions, newTransaction]);
+  };
+
+  const handleDeleteTransaction = (id) => {
+    const updatedTransactions = filteredTransactions.filter((transaction) => transaction.id !== id);
+    setFilteredTransactions(updatedTransactions);
+  };
+
+  const handleFilterTransactions = (searchTerm) => {
+    const filtered = transactions.filter((transaction) =>
+      transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredTransactions(filtered);
+  };
+
+  return (
+    <div>
+      <h1>Bank Transactions App</h1>
+      <TransactionForm onAddTransaction={handleAddTransaction} />
+      <div>
+        <label>Search:</label>
+        <input type="text" onChange={(e) => handleFilterTransactions(e.target.value)} />
+      </div>
+      <TransactionTable transactions={filteredTransactions} onDeleteTransaction={handleDeleteTransaction} />
+    </div>
+  );
+};
+
+export default App;
